8a6a1995fc5d6b4e3054fd47382eb8c7fb8ab6e2
11Alex111 <xalexey.g@gmail.com>
2023-08-20 16:50:41 +0300

    last update hw-07-js-file-01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,100 +25,19 @@ allGallery.addEventListener('click', (event) => {
    event.preventDefault();
    if (event.target.nodeName === 'IMG') {
        const originalImgURL = event.target.getAttribute('data-source');
-       const instance = basicLightbox.create(`
-           <img src="${originalImgURL}" width="1280">
-       `);
-    instance.show();
-     window.addEventListener('keydown', closeModal);
-    function closeModal(event) {
-      if (event.key === 'Escape') {
-        instance.close();
-     window.removeEventListener('keydown', closeModal);
-  };
-};
-  };
+     const instance = basicLightbox.create(`<img src="${originalImgURL}" width="1280">`,
+       {
+           onShow: () => {
+               window.addEventListener('keydown', onCloseLightbox);
+           },
+           onClose: () => {
+               window.removeEventListener('keydown', onCloseLightbox);}
+       });
+       const onCloseLightbox = (event) => {
+           if (event.key === 'Escape') {
+               instance.close();
+           }
+       };
+       instance.show();
+   }
 });
-
-
-
- 
-
-
-
-
-
-
-
-// const insertListItems = (string) => { 
-//     allGallery.insertAdjacentHTML('beforeend', string);
-// };
-
-// insertListItems(renderList(galleryItems));
-
-
-// document.querySelector('.gallery').onclick = (event) => {
-//   event.preventDefault();
-//   const image = galleryItems.map(({original}) => `${original}`);
-//   basicLightbox.create(`
-// 		<img width="1400" height="900" src="${image}">
-// 	`).show()
-// };
-
-
-
-
-// allGallery.addEventListener('click', (event) => {
-//   if (event.target.nodeName === 'IMG') {
-//     event.preventDefault();
-//   }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// allGallery.addEventListener('click', openImg);
-
-
-// const openImg = () => {
-//   return basicLightbox.create(`
-// 		<img width="1400" height="900" src="${galleryItems.original}">
-// 	`).show();
-// };
-
-
-
-
-
-
-
-
-
-// const instance = basicLightbox.create(`
-//     <li class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </li>
-// `)
-
-// instance.show()
-
-
-
-
-
-// console.log(galleryItems);
